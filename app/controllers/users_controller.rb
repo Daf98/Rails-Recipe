2e4173e984d8736dded8358ec0b1cd41f44896b5
Elson Otake <elsonotake@yahoo.com.br>
2022-10-03 16:05:47 -0300

    Add user index.html

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  # before_action :authenticate_user!, except: [:index]
+  # load_and_authorize_resource
+
+  def index
+  end
+
+  def show
+  end
+end
