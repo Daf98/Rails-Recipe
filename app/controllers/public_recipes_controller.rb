7987a660f3f73baf8df20883755d6c35840199a4
Daf <dafne.azzolina@gmail.com>
2022-10-04 13:39:23 -0300

    Fix linters

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,13 +1,13 @@
 class PublicRecipesController < ApplicationController
-    before_action :authenticate_user!
-    def index
-      @recipes = Recipe.where(public: :true).order(created_at: :desc)
-    end
-  
-    def destroy
-      @recipe = Recipe.find(params[:id])
-      @recipe.delete
-      flash[:success] = 'The recipe was successfully deleted.'
-      redirect_to recipes_path
-    end
+  before_action :authenticate_user!
+  def index
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    flash[:success] = 'The recipe was successfully deleted.'
+    redirect_to recipes_path
+  end
+end
