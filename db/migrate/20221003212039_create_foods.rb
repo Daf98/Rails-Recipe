031bb71aa086d4f86043a90a0df100d189c1e4c0
Daf <dafne.azzolina@gmail.com>
2022-10-05 16:47:18 -0300

    Fix merge conflicts

--- db/migrate/20221003212039_create_foods.rb
+++ db/migrate/20221003212039_create_foods.rb
@@ -1,8 +1,9 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :name
-      t.string :measurement
+      t.string :measurement_unit
       t.integer :price
       t.integer :quantity
 
